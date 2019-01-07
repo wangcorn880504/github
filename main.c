86a4374e89467496720446cabf84c168b10d3fc6
yumin_wang <wangcorn880504@gmail.com>
2019-01-07 15:15:46 +0800

    加上if控制

--- main.c
+++ main.c
@@ -4,10 +4,17 @@
 int main()
 {
     int i;
+    int j;
     for(i= 0;i<10;i++){
     printf("Hello world!\n");
+    printf("%x\n",&i);
     if(i == 9){
         printf("Za warudo!\n");
+        if(j != 1){
+        i = 0;
+        j = 1;
+        }
+
     }
     }
     return 0;
